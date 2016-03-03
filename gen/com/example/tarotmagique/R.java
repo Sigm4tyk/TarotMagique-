539807a12e26544d941e3c218b64e546cf5fba06
sigmatyk1 <jperrin1992@gmail.com>
2016-03-03 14:45:38 +0100

    version 6

--- gen/com/example/tarotmagique/R.java
+++ gen/com/example/tarotmagique/R.java
@@ -25,12 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080009;
         public static final int buttonHeberger=0x7f080002;
-        public static final int buttonJouer=0x7f080005;
+        public static final int buttonJouer=0x7f080007;
         public static final int buttonJoueur=0x7f080004;
-        public static final int buttonListerAtouts=0x7f080006;
+        public static final int buttonListerAtouts=0x7f080008;
         public static final int buttonRejoindrePartie=0x7f080001;
+        public static final int editNomTable=0x7f080005;
+        public static final int editNombreJoueurs=0x7f080006;
         public static final int listViewAtouts=0x7f080000;
         public static final int textViewBienvenue=0x7f080003;
     }
@@ -50,12 +52,14 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int bienvenue=0x7f050001;
         public static final int buttonHeberger=0x7f050003;
-        public static final int buttonJouer=0x7f05000e;
+        public static final int buttonJouer=0x7f050010;
         public static final int buttonListerAtouts=0x7f050009;
         public static final int buttonPiocher=0x7f050006;
         public static final int buttonRejoindrePartie=0x7f050004;
         public static final int checkBoxExcuse=0x7f050007;
         public static final int derniereCartePiochee=0x7f050005;
+        public static final int editNomTable=0x7f05000e;
+        public static final int editNombreJoueurs=0x7f05000f;
         public static final int labelActivityParametresConnexionPartie=0x7f05000b;
         public static final int labelActivityParametresHeberger=0x7f05000c;
         public static final int labelActivityRegarderAtouts=0x7f05000d;
